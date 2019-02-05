e4f65fad933a02d522d04c86946f6e20e9690d7e
arjun <gupta.arjun45@gmail.com>
2019-02-05 17:39:57 -0500

    Updated

--- lab3/aes_cfa.py
+++ lab3/aes_cfa.py
@@ -4,7 +4,21 @@ import os
 import sys
 
 
+def find_data_file():
+    subdir = "data"
+    path = os.path.normpath(os.path.join(os.getcwd(), subdir))
+    os.chdir(path)
+    files = [f for f in os.listdir(os.getcwd()) if os.path.isfile(f)]
+    print('Current files:')
+    print("-------------------------------------------------------------")
+    [print("{}. {}".format(f+1, files[f])) for f in enumerate(files)]
+    # for i in range(0, len(files)):
+    #     print("{}. {}".format(i + 1, files[i]))
+    print("-------------------------------------------------------------")
 
+
+    return 0
+    
 
 
 
