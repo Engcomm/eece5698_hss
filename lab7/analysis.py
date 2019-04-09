a9d3a7e909daa38c02f3b1466b44dd4355425049
agupta1 <gupta.arj@husky.neu.edu>
2019-04-09 17:24:06 -0400

    update 2

--- lab7/analysis.py
+++ lab7/analysis.py
@@ -2,6 +2,23 @@
 
 import sys
 import socket
+import time
+import struct
+
+SERVER_IP = '10.75.8.54'
+# SERVER_IP = '127.0.0.1'
+SERVER_PORT = 9907
+# SERVER_PORT = 3333 
 
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((SERVER_IP, SERVER_PORT))
 
+
+msg = struct.pack('HH', 1, 100)
+s.send(msg)
+s.send('A'*100)
+
+msg = s.recv(100)
+s.close()
+print(msg)
